3dd04b9b8a4915b1b0470cdd36794ca38040660e
Marcin S <macest77@gmail.com>
2022-06-05 18:19:28 +0200

    routes update

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 use App\Http\Controllers\LottoController;
+use App\Http\Controllers\MusicController;
+use App\Http\Controllers\StandingsController;
 // LottoController
 Route::get('/lotto', [LottoController::class, 'index']);
 Route::get('/lotto/wyniki', [LottoController::class, 'getdraw']);
 Route::get('/lotto/getdrawdate', [LottoController::class, 'getdrawdate']);
 Route::get('/lotto/przewidywania', [LottoController::class, 'countnext']);
+Route::get('/reviews', [MusicController::class, 'index']);
+Route::get('/lista-hard-n-heavy', [MusicController::class, 'list']);
+Route::post('/lista-hard-n-heavy', [MusicController::class, 'listpost']);
+Route::get('/confirmcode', [MusicController::class, 'confirmCode']);
+Route::get('/chart/{listAdminPass}', [MusicController::class, 'chart']);
+Route::post('/addsong', [StandingsController::class, 'addsongpost']);
+Route::get('/addsong', [StandingsController::class, 'addsong']);
+Route::get('/list/{id}', [MusicController::class, 'listing']);
